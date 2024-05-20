ee4a100c4760fac6cc10d6e5a7ac1beb09bb5d68
breno-ca <breno_de_avila@outlook.com>
2024-05-20 14:27:34 -0300

    feat: add nvim-treesitter for better code syntax highlighting

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,20 @@
+return {
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+
+	config = function()
+
+    	local configs = require'nvim-treesitter.configs'
+
+    	configs.setup{
+			ensure_installed = {
+				'dockerfile', 'gitignore', 'go', 'gomod', 'gowork',
+				'javascript', 'json', 'lua', 'markdown', 'proto',
+				'python', 'ruby', 'sql', 'yaml'
+			},
+        	sync_install = false,
+        	highlight = { enable = true },
+        	indent = { enable = true },
+        }
+    end
+}
