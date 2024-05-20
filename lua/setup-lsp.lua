2a2f73ccd737ea1f68cd751dd5693dbfdcb5b5a5
breno-ca <breno_de_avila@outlook.com>
2024-05-20 14:31:11 -0300

    feat: add setup-lsp as base lsp configuration file

---
+++ lua/setup-lsp.lua
@@ -0,0 +1,57 @@
+local lspconfig = require'lspconfig'
+local util = require'lspconfig/util'
+
+local capabilities = require'cmp_nvim_lsp'.default_capabilities()
+local on_attach = require'lsp-format'.on_attach
+
+lspconfig.gopls.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+
+	cmd = {"gopls"},
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+
+	settings = {
+		gopls = {
+			analyses = {
+				nilness = true,
+				unusedparams = true,
+			},
+			completeUnimported = true,
+			gofumpt = true,
+			staticcheck = true,
+			hints = {
+				assignVariableTypes = true,
+				parameterNames = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+			},
+		},
+	},
+})
+
+lspconfig.lua_ls.setup({})
+
+lspconfig.bashls.setup({})
+
+lspconfig.jsonls.setup({})
+
+lspconfig.yamlls.setup({})
+
+--[[
+Configuração de LSPs (Language Server Protocols)
+
+Cada linguagem a ser utilizada deve conter uma
+chamada setup() para funcionar contendo ou não
+uma table de configurações. 
+
+Para listar os LSPs disponíveis execute :Mason.
+
+Para instalar um LSP da lista basta escrever
+:MasonInstall informando o nome do LSP desejado
+
+A lista de compatibilidade e configurações se
+encontra em:
+https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#bashls
+--]]
