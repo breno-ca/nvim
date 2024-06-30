b90701a4009335f1910ac6a594d7788a78fb29f5
breno-ca <breno_de_avila@outlook.com>
2024-06-30 14:47:51 -0300

    chore: add nofixendofline, add new colorschemes, nvim-tree now starts unfocused

--- init.lua
+++ init.lua
@@ -3,13 +3,15 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.laststatus = 3
 vim.opt.termguicolors = true
-vim.o.number = true
-vim.o.linebreak = true
 vim.g.floaterm_height = 0.9
 vim.g.floaterm_width = 0.9
+vim.cmd('set nofixendofline')
+vim.cmd('set linebreak')
+vim.cmd('set number')
 -- vim.opt.noequalalways = true
 
 vim.cmd('autocmd VimEnter * NvimTreeOpen')
+vim.cmd('autocmd VimEnter * wincmd w')
 
 -- configurações recomendadas para o nvim-tree
 vim.g.loaded_netrw = 1
@@ -18,34 +20,37 @@ vim.g.loaded_netrwPlugin = 1
 -- lazy.nvim para gerenciamento de plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- carrega os plugins da pasta
-require'lazy'.setup('plugins')
+require 'lazy'.setup('plugins')
 
 -- carrega os atalhos globais personalizados
-require'shortcuts'
+require 'shortcuts'
 
 -- carrega as funções para alterar a fonte
-require'fonts'
+require 'fonts'
 
 -- carrega os setups necessários
-require'setup'
+require 'setup'
 
 -- carrega o suporte a linguagens
-require'setup-lsp'
+require 'setup-lsp'
 
 -- carrega as configurações de debug
-require'setup-dap'
+require 'setup-dap'
 
 -- carrega o tema
-vim.cmd.colorscheme'tokyonight-night'
+vim.cmd.colorscheme 'tokyonight-night'
+-- vim.cmd.colorscheme 'night-owl'; vim.opt.fillchars:append { eob = ' ' }
+-- vim.cmd.colorscheme 'NeoSolarized'; vim.opt.fillchars:append { eob = ' ' }
+-- vim.cmd.colorscheme 'hivacruz'
