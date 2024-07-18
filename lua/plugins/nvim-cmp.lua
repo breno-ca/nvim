d0ced41731be652607a5ff745b52b91070bc8b58
breno-ca <breno_de_avila@outlook.com>
2024-07-17 21:42:51 -0300

    fix: now jump to the next field work on snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,5 +1,5 @@
 return {
-    'hrsh7th/nvim-cmp',
+	'hrsh7th/nvim-cmp',
 
 	event = 'InsertEnter',
 
@@ -7,34 +7,53 @@ return {
 		'hrsh7th/cmp-nvim-lsp',
 		'L3MON4D3/LuaSnip',
 		'saadparwaiz1/cmp_luasnip',
-    },
+	},
 
-    config = function()
-    	local cmp = require'cmp'
+	config = function()
+		local cmp = require 'cmp'
+		local luasnip = require 'luasnip'
 
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					require'luasnip'.lsp_expand(args.body)
+					require 'luasnip'.lsp_expand(args.body)
 				end,
-    		},
-    		window = {
+			},
+			window = {
 				completion = cmp.config.window.bordered(),
 				documentation = cmp.config.window.bordered(),
 			},
-    		mapping = cmp.mapping.preset.insert({
+			mapping = cmp.mapping.preset.insert({
 				['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    			['<C-d>'] = cmp.mapping.scroll_docs(4),
-    			['<C-Space>'] = cmp.mapping.complete(),
-    			['<C-e>'] = cmp.mapping.abort(),
-    			['<CR>'] = cmp.mapping.confirm({ select = true }),
-    		}),
-		    sources = cmp.config.sources({
+				['<C-d>'] = cmp.mapping.scroll_docs(4),
+				['<C-Space>'] = cmp.mapping.complete(),
+				['<C-e>'] = cmp.mapping.abort(),
+				['<CR>'] = cmp.mapping.confirm({ select = true }),
+				['<Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+				['<S-Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+			}),
+			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-    			{ name = 'luasnip' },
+				{ name = 'luasnip' },
 			}, {
 				{ name = 'buffer' },
 			})
 		})
-    end
+	end
 }
