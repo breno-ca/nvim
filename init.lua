a775719f7acabbfff22c762f43aee9345c60b2a3
breno-ca <breno_de_avila@outlook.com>
2024-07-17 22:01:42 -0300

    feat: add python3.12 provider

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.opt.laststatus = 3
 vim.opt.termguicolors = true
 vim.g.floaterm_height = 0.9
 vim.g.floaterm_width = 0.9
+vim.g.python3_host_prog = '/usr/local/bin/python3.12'
 vim.cmd('set nofixendofline')
 vim.cmd('set linebreak')
 vim.cmd('set number')
@@ -18,21 +19,21 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- lazy.nvim para gerenciamento de plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- carrega os plugins da pasta
-require 'lazy'.setup('plugins')
+require 'lazy'.setup 'plugins'
 
 -- carrega os atalhos globais personalizados
 require 'shortcuts'
@@ -48,9 +49,3 @@ require 'setup-lsp'
 
 -- carrega as configurações de debug
 require 'setup-dap'
-
--- carrega o tema
-vim.cmd.colorscheme 'tokyonight-night'
--- vim.cmd.colorscheme 'night-owl'; vim.opt.fillchars:append { eob = ' ' }
--- vim.cmd.colorscheme 'NeoSolarized'; vim.opt.fillchars:append { eob = ' ' }
--- vim.cmd.colorscheme 'hivacruz'
