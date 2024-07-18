30d60b673854119cbdf3c63bfd19491dfbb964ac
breno-ca <breno_de_avila@outlook.com>
2024-07-17 21:40:49 -0300

    feat: add hurl for easy http requests and tests

---
+++ lua/plugins/hurl.lua
@@ -0,0 +1,22 @@
+return {
+	lazy = false,
+	'jellydn/hurl.nvim',
+	dependencies = {
+		'MunifTanjim/nui.nvim',
+		'nvim-lua/plenary.nvim',
+		'nvim-treesitter/nvim-treesitter'
+	},
+	ft = 'hurl',
+	opts = {
+
+		debug = false,
+
+		show_notification = false,
+		mode = 'split',
+		formatters = {
+			json = { 'jq' },
+			html = { 'prettier', '--parser', 'html' },
+			xml = { 'tidy', '-xml', '-i', '-q' },
+		},
+	},
+}
