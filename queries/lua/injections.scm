39113ad107ebae1a2a17b6d79963221e2ec5100f
breno-ca <breno_de_avila@outlook.com>
2024-11-06 12:40:10 -0300

    feat: added vim highlight injections in vim.cmd() arguments

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,8 +1,8 @@
 (expression_list
   value: (function_call
     name: (dot_index_expression
-      field: (identifier) @field
-      (#eq? @field "parse_query")
+      field: (identifier) @query_field
+      (#eq? @query_field "parse_query")
     )
     arguments: (arguments
       (string
@@ -12,6 +12,18 @@
         content: (string_content) @injection.content
         (#set! injection.language "query")
       )
+    )
+  )
+)
+
+(function_call
+  name: (dot_index_expression
+    field: (identifier) @cmd_field)
+    (#eq? @cmd_field "cmd")
+  arguments: (arguments
+    (string
+      content: (string_content) @injection.content
+      (#set! injection.language "vim")
     )
   )
 )
