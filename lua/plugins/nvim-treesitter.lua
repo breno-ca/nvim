25057fb6268f4997cc768cdbde769b0b7c94054a
breno-ca <breno_de_avila@outlook.com>
2024-12-06 18:12:32 -0300

    fix: disabled nvim default syntax highlighting

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -7,12 +7,10 @@ return {
 		local configs = require 'nvim-treesitter.configs'
 		local api = vim.api -- Add this line to access Neovim API
 
-		-- Function to get the language of a buffer
 		local function get_language(bufnr)
 			return vim.api.nvim_buf_get_option(bufnr, 'filetype')
 		end
 
-		-- Table of functions for each language
 		local language_functions = {
 			html = function(bufnr)
 				local lines = api.nvim_buf_get_lines(bufnr, 0, -1, false)
@@ -23,7 +21,15 @@ return {
 				end
 				return false
 			end,
-			-- Add more languages and their corresponding functions as needed
+			json = function(bufnr)
+				local lines = api.nvim_buf_get_lines(bufnr, 0, -1, false)
+				for _, line in ipairs(lines) do
+					if #line > 2900 then
+						return true
+					end
+				end
+				return false
+			end,
 		}
 
 		configs.setup {
@@ -39,18 +45,15 @@ return {
 			indent = { enable = true },
 			highlight = {
 				enable = true,
+				additional_vim_regex_highlighting = false,
+
 				disable = function(lang, bufnr)
 					local current_lang = get_language(bufnr)
-
-					-- Look up the function for the current language in the table
 					local func = language_functions[current_lang]
 
-					-- If a function was found, call it and return its result
 					if func then
 						return func(bufnr)
 					end
-
-					-- If no function was found for the current language, return false
 					return false
 				end,
 			},
