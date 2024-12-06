a00b4aedd664ec549f7381fefaebb7963ebe64ef
breno-ca <breno_de_avila@outlook.com>
2024-12-06 17:57:48 -0300

    change: now the response is displayed in popup mode

--- lua/plugins/hurl.lua
+++ lua/plugins/hurl.lua
@@ -1,18 +1,20 @@
 return {
-	lazy = false,
 	'jellydn/hurl.nvim',
+	lazy = false,
+	ft = 'hurl',
+
 	dependencies = {
 		'MunifTanjim/nui.nvim',
 		'nvim-lua/plenary.nvim',
 		'nvim-treesitter/nvim-treesitter'
 	},
-	ft = 'hurl',
+
 	opts = {
-
 		debug = false,
-
+		mode = 'popup',
 		show_notification = false,
-		mode = 'split',
+		file_root = vim.fn.getcwd(),
+
 		formatters = {
 			json = { 'jq' },
 			html = { 'prettier', '--parser', 'html' },
