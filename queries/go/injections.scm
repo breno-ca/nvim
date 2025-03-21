d18b8fe325a74386de28067fc9892d681b350bad
breno-ca <breno_de_avila@outlook.com>
2025-03-21 16:33:16 -0300

    fix: updated highlight injection to be compatible with the new nvim version

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -2,11 +2,10 @@
 ; In strings
 (
   [
-    (raw_string_literal)
-    (interpreted_string_literal)
+    (raw_string_literal_content)
+    ;(interpreted_string_literal_content)
   ] @injection.content
-
-  (#offset! @injection.content 0 1 0 -1)
+  ;(#offset! @injection.content 0 1 0 -1)
   (#match? @injection.content
     "(SELECT|INSERT|REPLACE|UPDATE|DELETE|WHERE|JOIN).+(FROM|VALUES|SET|ON)?"
   )
@@ -16,12 +15,10 @@
 ;;; JSON Highlight
 ; In strings
 (
-  (expression_list
-    (raw_string_literal) @injection.content
+  (raw_string_literal_content) @injection.content
 
-    (#match? @injection.content ".*[{\\[\\]}:,\"].*")
-    (#set! injection.language "json")
-  )
+  (#match? @injection.content ".*[{\\[\\]}:,\"].*")
+  (#set! injection.language "json")
 )
 
 ;;XML highlight injection for string literals
