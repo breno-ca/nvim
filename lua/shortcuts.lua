4560a59d7710d3023caf49b0f30dc2c6d8c36e7c
breno-ca <breno_de_avila@outlook.com>
2025-03-21 16:30:32 -0300

    change: replaced some shortcuts that use <alt> with <leader> as the base

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -6,11 +6,6 @@ local function nsd(description)
 	return { noremap = true, silent = true, desc = description }
 end
 
--- noremap, silent, description, expression, replace keycodes
-local function nsder(description)
-	return { noremap = true, silent = true, desc = description, expr = true, replace_keycodes = false }
-end
-
 -- function caller with arguments
 local function call(func, args)
 	return function() func(args) end
@@ -21,154 +16,141 @@ local n = 'n'
 local i = 'i'
 local t = 't'
 local v = 'v'
-local ni = { 'n', 'i' }
-local nv = { 'n', 'v' }
+local ni = { n, i }
+local nv = { n, v }
 local xo = { 'x', 'o' }
-local ntv = { 'n', 't', 'v' }
-local niv = { 'n', 'i', 'v' }
--- local nitv = {'n', 'i', 't', 'v'}
+local ntv = { n, t, v }
+local niv = { n, i, v }
 
+local keymap = function(mode, mapping, command, opts)
+	vim.keymap.set(mode, mapping, command, opts)
+end
 
 -----------------------------------------------
 -- neovim configurations
 
 -- remapings
-vim.keymap.set(n, '<Up>', 'gk', nsd('Move UP through virtual lines in NORMAL mode'))
-vim.keymap.set(n, '<Down>', 'gj', nsd('Move DOWN through virtual lines in NORMAL mode'))
-vim.keymap.set(i, '<Up>', '<C-o>gk', nsd('Move UP through virtual lines in INSERT mode'))
-vim.keymap.set(i, '<Down>', '<C-o>gj', nsd('Move DOWN through virtual lines in INSERT mode'))
+keymap(n, '<Up>', 'gk', nsd('Move UP through virtual lines in NORMAL mode'))
+keymap(n, '<Down>', 'gj', nsd('Move DOWN through virtual lines in NORMAL mode'))
+keymap(i, '<Up>', '<C-o>gk', nsd('Move UP through virtual lines in INSERT mode'))
+keymap(i, '<Down>', '<C-o>gj', nsd('Move DOWN through virtual lines in INSERT mode'))
+keymap(n, ';', '/', nsd('Search forward in NORMAL mode'))
+keymap(i, '<Esc>', '<Esc><Cmd>w<CR>', nsd('Save file on exit from INSERT mode'))
 
 -----------------------------------------------
 -- shortcuts
 
 -- neovim actions
-vim.keymap.set(n, '<Leader>vl', '<Cmd>set wrap!<CR>', nsd('Toggle line wrapping'))
+keymap(n, '<Leader>vl', '<Cmd>set wrap!<CR>', nsd('Toggle line wrapping'))
 
 -- code actions
-vim.keymap.set(n, '<A-i>', call(vim.diagnostic.open_float), nsd('Inspect diagnostics'))
-vim.keymap.set(ni, '<A-a>', call(vim.lsp.buf.code_action), nsd('Show code actions menu'))
+keymap(n, '<A-i>', call(vim.diagnostic.open_float), nsd('Inspect diagnostics'))
+keymap(ni, '<A-a>', call(vim.lsp.buf.code_action), nsd('Show code actions menu'))
 -- moving lines
-vim.keymap.set(n, '<A-k>', 'ddkP', nsd('Move current line UP in NORMAL mode'))
-vim.keymap.set(n, '<A-j>', 'ddp', nsd('Move current line DOWN in NORMAL mode'))
-vim.keymap.set(i, '<A-k>', '<Esc>ddkPi', nsd('Move current line UP in INSERT mode'))
-vim.keymap.set(i, '<A-j>', '<Esc>ddpi', nsd('Move current line UP in INSERT mode'))
+keymap(n, '<A-k>', 'ddkP', nsd('Move current line UP in NORMAL mode'))
+keymap(n, '<A-j>', 'ddp', nsd('Move current line DOWN in NORMAL mode'))
+keymap(i, '<A-k>', '<Esc>ddkPi', nsd('Move current line UP in INSERT mode'))
+keymap(i, '<A-j>', '<Esc>ddpi', nsd('Move current line UP in INSERT mode'))
 -- adding lines
-vim.keymap.set(i, '<A-CR>', '<C-o>o', nsd('Add a new line below in INSERT mode'))
-vim.keymap.set(i, '<A-]>', '<C-o>O', nsd('Add a new line in INSERT mode'))
+keymap(i, '<A-CR>', '<C-o>o', nsd('Add a new line below in INSERT mode'))
+keymap(i, '<A-]>', '<C-o>O', nsd('Add a new line in INSERT mode'))
 -- formatting
-vim.keymap.set(v, '<Leader>fj', ':!jq .<CR>', nsd('Format JSON in visual mode'))
+keymap(v, '<Leader>fj', ':!jq .<CR>', nsd('Format JSON in visual mode'))
 
 -- buffer actions
-vim.keymap.set(niv, '<A-z>', '<Cmd>undo<CR>', nsd('Undo last change'))
-vim.keymap.set(niv, '<A-Z>', '<Cmd>redo<CR>', nsd('Redo last change'))
-vim.keymap.set(niv, '<A-s>', '<Cmd>w<CR>', nsd('Save changes in current file'))
+keymap(niv, '<C-z>', '<Cmd>undo<CR>', nsd('Undo last change'))
+keymap(niv, '<C-Z>', '<Cmd>redo<CR>', nsd('Redo last change'))
+keymap(niv, '<C-s>', '<Cmd>w<CR>', nsd('Save changes in current file'))
+keymap(nv, '<Leader><Esc>', '<Cmd>w<CR>', nsd('Save changes in current file'))
 -- buffer splitting
-vim.keymap.set(n, '<Leader>s', '<Cmd>vsplit<CR>', nsd('Split buffer window vertically'))
-vim.keymap.set(n, '<Leader>x', '<Cmd>split<CR>', nsd('Split buffer window horizontally'))
-vim.keymap.set(n, '<Leader>S', '<Cmd>wincmd x<CR>', nsd('Swap buffer windows'))
+keymap(n, '<Leader>sd', '<Cmd>vsplit<CR><Cmd>wincmd l<CR>', nsd('Split buffer window vertically'))
+keymap(n, '<Leader>sx', '<Cmd>split<CR><Cmd>wincmd j<CR>', nsd('Split buffer window horizontally'))
+keymap(n, '<Leader>S', '<Cmd>wincmd x<CR>', nsd('Swap buffer windows'))
 -- buffer sizing
-vim.keymap.set(n, '<A-->', '<C-w><', nsd('Decrease buffer window width'))
-vim.keymap.set(n, '<A-=>', '<C-w>>', nsd('Increase buffer window width'))
-vim.keymap.set(n, '<A-_>', '<C-w>-', nsd('Decrease buffer window height'))
-vim.keymap.set(n, '<A-+>', '<C-w>+', nsd('Increase buffer window height'))
+keymap(n, '<A-->', '<C-w><', nsd('Decrease buffer window width'))
+keymap(n, '<A-=>', '<C-w>>', nsd('Increase buffer window width'))
+keymap(n, '<A-S-->', '<C-w>-', nsd('Decrease buffer window height'))
+keymap(n, '<A-S-=>', '<C-w>+', nsd('Increase buffer window height'))
 -- buffer movement
-vim.keymap.set(n, '<Leader><Leader>', '<Cmd>wincmd w<CR>', nsd('Cycle through buffers'))
-vim.keymap.set(n, '<Leader><Up>', '<Cmd>wincmd k<CR>', nsd('Move to the buffer above'))
-vim.keymap.set(n, '<Leader><Down>', '<Cmd>wincmd j<CR>', nsd('Move to the buffer below'))
-vim.keymap.set(n, '<Leader><Left>', '<Cmd>wincmd h<CR>', nsd('Move to the buffer on the left'))
-vim.keymap.set(n, '<Leader><Right>', '<Cmd>wincmd l<CR>', nsd('Move to the buffer on the right'))
-vim.keymap.set(n, '<Leader>wq', '<Cmd>wincmd q<CR>', nsd('Close buffer window'))
+keymap(n, '<Leader><Leader>', '<Cmd>wincmd w<CR>', nsd('Cycle through buffers'))
+keymap(n, '<Leader><Up>', '<Cmd>wincmd k<CR>', nsd('Move to the buffer above'))
+keymap(n, '<Leader><Down>', '<Cmd>wincmd j<CR>', nsd('Move to the buffer below'))
+keymap(n, '<Leader><Left>', '<Cmd>wincmd h<CR>', nsd('Move to the buffer on the left'))
+keymap(n, '<Leader><Right>', '<Cmd>wincmd l<CR>', nsd('Move to the buffer on the right'))
+keymap(n, '<Leader>qw', '<Cmd>wincmd q<CR>', nsd('Close buffer window'))
+
+-- quit
+keymap(n, '<Leader>qa', '<Cmd>qa<CR>', nsd('Safe quit'))
 
 -----------------------------------------------
 -- plugins
 
 -- nvim-tree
-vim.keymap.set(n, '<A-w>', '<Cmd>NvimTreeFocus<CR>', nsd('Put the cursor in files tree'))
-vim.keymap.set(n, '<A-S-w>', '<Cmd>NvimTreeToggle<CR>', nsd('Toggle files tree visibility'))
+keymap(n, '<Leader>w', '<Cmd>NvimTreeOpen<CR>', nsd('Toggle files tree visibility'))
 
 -- telescope
 local telescope = require 'telescope.builtin'
-vim.keymap.set(n, '<Leader>e', telescope.find_files, nsd('Search files by name'))
-vim.keymap.set(n, '<Leader>E', telescope.git_status, nsd('Search through git status'))
-vim.keymap.set(n, '<Leader>f', telescope.live_grep, nsd('Search files with live grep'))
-vim.keymap.set(n, '<Leader>u', telescope.lsp_references, nsd('Show LSP references'))
-vim.keymap.set(n, '<Leader><CR>', telescope.lsp_implementations, nsd('Show LSP implementation'))
+keymap(n, '<Leader>f', telescope.find_files, nsd('Search files by name'))
+keymap(n, '<Leader>gf', telescope.live_grep, nsd('Search files with live grep'))
+keymap(n, '<Leader>gs', telescope.git_status, nsd('Search through git status'))
+keymap(n, '<Leader>sr', telescope.lsp_references, nsd('Show LSP references'))
+keymap(n, '<Leader><CR>', telescope.lsp_implementations, nsd('Show LSP implementation'))
 
 -- barbar tabline
-vim.keymap.set(n, '<A-,>', '<Cmd>BufferPrevious<CR>', nsd('Go to the next tab'))
-vim.keymap.set(n, '<A-.>', '<Cmd>BufferNext<CR>', nsd('Go to the previous tab'))
-vim.keymap.set(n, '<A-<>', '<Cmd>BufferMovePrevious<CR>', nsd('Move the current tab to the left'))
-vim.keymap.set(n, '<A->>', '<Cmd>BufferMoveNext<CR>', nsd('Move the current tab to the right'))
-vim.keymap.set(n, '<A-q>', '<Cmd>BufferClose<CR>', nsd('Close the current tab'))
+keymap(n, '<A-,>', '<Cmd>BufferPrevious<CR>', nsd('Go to the next tab'))
+keymap(n, '<A-.>', '<Cmd>BufferNext<CR>', nsd('Go to the previous tab'))
+keymap(n, '<A-S-,>', '<Cmd>BufferMovePrevious<CR>', nsd('Move the current tab to the left'))
+keymap(n, '<A-S-.>', '<Cmd>BufferMoveNext<CR>', nsd('Move the current tab to the right'))
+keymap(n, '<Leader>qq', '<Cmd>BufferClose<CR>', nsd('Close the current tab'))
 
 -- trouble
-vim.keymap.set(n, '<Leader>ww', '<Cmd>Trouble diagnostics toggle focus=true<CR>', nsd('Show workspace diagnostics'))
-vim.keymap.set(n, '<Leader>wd', '<Cmd>Trouble diagnostics toggle focus=true filter.buf=0<CR>',
+keymap(n, '<Leader>tw', '<Cmd>Trouble diagnostics toggle focus=true<CR>', nsd('Show workspace diagnostics'))
+keymap(n, '<Leader>td', '<Cmd>Trouble diagnostics toggle focus=true filter.buf=0<CR>',
 	nsd('Show current document diagnostics'))
-vim.keymap.set(n, '<Leader>qf', '<Cmd>Trouble quickfix toggle focus=true<CR>', nsd('Show quickfixes'))
-
--- nvim-dap
-local dap = require 'dap'
-local dapui = require 'dapui'
-local widgets = require 'dap.ui.widgets'
-local loginput = function() dap.set_breakpoint(nil, nil, vim.fn.input('Log message: ')) end
-vim.keymap.set(n, '<A-S-D>', call(dapui.toggle), nsd('Show/Hide Debug Window Visibility'))
-vim.keymap.set(n, '<F4>', call(dap.run_last), nsd('Restart Last Debug Session'))
-vim.keymap.set(n, '<F5>', call(dap.continue), nsd('Continue/Start Debug Session'))
-vim.keymap.set(n, '<F6>', call(dap.restart), nsd('Restart Debug Session'))
-vim.keymap.set(n, '<F10>', call(dap.step_over), nsd('Step Over'))
-vim.keymap.set(n, '<F11>', call(dap.step_into), nsd('Step Into'))
-vim.keymap.set(n, '<F12>', call(dap.step_out), nsd('Step Out'))
-vim.keymap.set(n, '<Leader>b', call(dap.toggle_breakpoint), nsd('Toggle Breakpoint'))
-vim.keymap.set(n, '<Leader>lp', loginput, nsd('Toggle Logpoint'))
-vim.keymap.set(nv, '<Leader>vv', call(widgets.hover), nsd('Displays a variable value in a floating window'))
+keymap(n, '<Leader>tf', '<Cmd>Trouble quickfix toggle focus=true<CR>', nsd('Show quickfixes'))
 
 -- floaterm
-vim.keymap.set(ntv, '<A-\'>', '<Cmd>:FloatermToggle<CR>', nsd('Toggle terminal'))
-vim.keymap.set(ntv, '<A-3>', '<Cmd>:FloatermNew<CR>', nsd('Open a new terminal session'))
-vim.keymap.set(ntv, '<A-1>', '<Cmd>:FloatermPrev<CR>', nsd('Go to the next terminal'))
-vim.keymap.set(ntv, '<A-2>', '<Cmd>:FloatermNext<CR>', nsd('Go to the previous terminal'))
+keymap(ntv, '<A-\'>', '<Cmd>:FloatermToggle<CR>', nsd('Toggle terminal'))
+keymap(ntv, '<A-3>', '<Cmd>:FloatermNew<CR>', nsd('Open a new terminal session'))
+keymap(ntv, '<A-1>', '<Cmd>:FloatermPrev<CR>', nsd('Go to the next terminal'))
+keymap(ntv, '<A-2>', '<Cmd>:FloatermNext<CR>', nsd('Go to the previous terminal'))
+keymap(t, '<A-q>', '<Cmd>:FloatermKill<CR>', nsd('Kill the current terminal'))
 
 -- codeium
 local neocodeium = require 'neocodeium'
-vim.keymap.set(i, '<A-;>', call(neocodeium.accept), nsd('Apply suggestion'))
-vim.keymap.set(i, '<A-Right>', call(neocodeium.accept_word), nsd('Accept word from suggestion'))
-vim.keymap.set(i, '<A-Up>', call(neocodeium.cycle, -1), nsd('Cycle to the prevous suggestion'))
-vim.keymap.set(i, '<A-Down>', call(neocodeium.cycle_or_complete, 1), nsd('Cycle to the next suggestion'))
-vim.keymap.set(i, '<A-ç>', call(neocodeium.clear), nsd('Clear current suggestion'))
-vim.keymap.set(n, '<Leader>cc', '<Cmd>NeoCodeium chat<CR>', nsd('Open Codeium chat window'))
+keymap(i, '<A-;>', call(neocodeium.accept), nsd('Apply suggestion'))
+keymap(i, '<A-Right>', call(neocodeium.accept_word), nsd('Accept word from suggestion'))
+keymap(i, '<A-Up>', call(neocodeium.cycle, -1), nsd('Cycle to the prevous suggestion'))
+keymap(i, '<A-Down>', call(neocodeium.cycle_or_complete, 1), nsd('Cycle to the next suggestion'))
+keymap(i, '<A-ç>', call(neocodeium.clear), nsd('Clear current suggestion'))
+keymap(n, '<Leader>ncc', '<Cmd>NeoCodeium chat<CR>', nsd('Open Codeium chat window'))
 
 -- centerpad
-vim.keymap.set(n, '<Leader>cb', '<Cmd>Centerpad 46 46<CR>', nsd('Centerpad the current buffer'))
+keymap(n, '<Leader>cb', '<Cmd>Centerpad 46 46<CR>', nsd('Centerpad the current buffer'))
 
 -- hurl
 local hurl_set_env_file = function() vim.cmd('HurlSetEnvFile ' .. vim.fn.input('Environment file: ')) end
-vim.keymap.set(n, '<Leader>r', '<Cmd>HurlRunnerAt<CR>', nsd('Run API request'))
-vim.keymap.set(n, '<Leader>hv', '<Cmd>HurlManageVariable<CR>', nsd('Manage Hurl variables'))
-vim.keymap.set(n, '<Leader>hef', call(hurl_set_env_file), nsd('Set Hurl environment file'))
-vim.keymap.set(nv, '<Leader>hr', '<Cmd>HurlRunner<CR>', nsd('Run all requests'))
-vim.keymap.set(n, '<Leader>hte', '<Cmd>HurlRunnerToEntry<CR>', nsd('Run API request to entry'))
-vim.keymap.set(n, '<Leader>htm', '<Cmd>HurlToggleMode<CR>', nsd('Toggle Hurl mode'))
-vim.keymap.set(n, '<Leader>htv', '<Cmd>HurlVerbose<CR>', nsd('Run API in verbose mode'))
+keymap(n, '<Leader>r', '<Cmd>HurlRunnerAt<CR>', nsd('Run API request'))
+keymap(n, '<Leader>hv', '<Cmd>HurlManageVariable<CR>', nsd('Manage Hurl variables'))
+keymap(n, '<Leader>hef', call(hurl_set_env_file), nsd('Set Hurl environment file'))
+keymap(nv, '<Leader>hr', '<Cmd>HurlRunner<CR>', nsd('Run all requests'))
+keymap(n, '<Leader>hte', '<Cmd>HurlRunnerToEntry<CR>', nsd('Run API request to entry'))
+keymap(n, '<Leader>htm', '<Cmd>HurlToggleMode<CR>', nsd('Toggle Hurl mode'))
+keymap(n, '<Leader>htv', '<Cmd>HurlVerbose<CR>', nsd('Run API in verbose mode'))
 
 -- leap
-vim.keymap.set(n, 's', '<Plug>(leap)', nsd('Leap to a word'))
-vim.keymap.set(n, 'S', '<Plug>(leap-from-window)', nsd('Leap to a word from another window'))
-vim.keymap.set(xo, 's', '<Plug>(leap-forward)', nsd('Leap to a word forward '))
-vim.keymap.set(xo, 'S', '<Plug>(leap-backward)', nsd('Leap to a word backward'))
+keymap(n, 's', '<Plug>(leap)', nsd('Leap to a word'))
+keymap(n, 'S', '<Plug>(leap-from-window)', nsd('Leap to a word from another window'))
+keymap(xo, 's', '<Plug>(leap-forward)', nsd('Leap to a word forward '))
+keymap(xo, 'S', '<Plug>(leap-backward)', nsd('Leap to a word backward'))
 
 -- gitblame
-vim.keymap.set(n, '<Leader>gb', '<Cmd>GitBlameToggle<CR>', nsd('Toggle gitblame comments visibility'))
+keymap(n, '<Leader>gb', '<Cmd>GitBlameToggle<CR>', nsd('Toggle gitblame comments visibility'))
 
--- unused ------------
---
--- copilot
--- vim.keymap.set(i, '<A-;>', 'copilot#Accept("<CR>")', nsder('Apply suggestion'))
--- vim.keymap.set(i, '<A-Down>', 'copilot#Next()', nsder('Cycle to the next suggestion, if one is available'))
--- vim.keymap.set(i, '<A-Up>', 'copilot#Previous()', nsder('Cycle to the previous suggestion'))
--- vim.keymap.set(i, '<A-ç>', 'copilot#Suggest()', nsder('Explicitly request a suggestion'))
--- vim.keymap.set(i, '<A-Ç>', 'copilot#Dismiss()', nsder('Dismiss the current suggestion'))
+-- ccc
+keymap(n, '<Leader>cc', '<Cmd>CccPick<CR>', nsd('Color picker'))
+keymap(n, '<Leader>CC', '<Cmd>CccConvert<CR>', nsd('Convert color'))
 
--- copilot-chat
--- vim.keymap.set(nv, '<Leader>cc', '<Cmd>CopilotChatToggle<CR>', nsd('Toggle Copilot Chat'))
--- vim.keymap.set(n, '<Leader>cs', '<Cmd>CopilotChatStop<CR>', nsd('Stop Copilot Chat'))
+-- peek
+keymap(n, '<Leader>mdo', '<Cmd>PeekOpen<CR>', nsd('Peek open'))
+keymap(n, '<Leader>mdc', '<Cmd>PeekClose<CR>', nsd('Peek close'))
