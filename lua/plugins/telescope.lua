5bcdb82373755717f28803d56985e39dfc6e6a2d
breno-ca <breno_de_avila@outlook.com>
2025-06-03 01:37:29 -0300

    feat: added multi files opening function in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,19 +9,34 @@ return {
 
 	config = function()
 		local telescope = require 'telescope'
+		local select_one_or_multi = function(prompt_bufnr)
+			local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+			local multi = picker:get_multi_selection()
+			if not vim.tbl_isempty(multi) then
+				require('telescope.actions').close(prompt_bufnr)
+				for _, j in pairs(multi) do
+					if j.path ~= nil then
+						vim.cmd(string.format('%s %s', 'edit', j.path))
+					end
+				end
+			else
+				require('telescope.actions').select_default(prompt_bufnr)
+			end
+		end
 
-		telescope.setup({
+		telescope.setup {
 			defaults = {
 				filesize_limit = 2,
+
+				mappings = { i = { ['<CR>'] = select_one_or_multi } },
+
+				preview = { treesitter = { enable = true } },
 
 				layout_config = {
 					scroll_speed = 3,
 					horizontal = { preview_width = 0.60 }
 				},
 
-				preview = {
-					treesitter = { enable = true }
-				}
 			},
 			extensions = {
 				fzf = {
@@ -30,7 +45,7 @@ return {
 					case_mode = 'smart_case',
 				}
 			}
-		})
+		}
 
 		telescope.load_extension 'fzf'
 		telescope.load_extension 'luasnip'
