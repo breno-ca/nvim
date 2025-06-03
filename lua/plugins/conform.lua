2f0b065745bb62cce6268481d151ddaff0bd43a6
breno-ca <breno_de_avila@outlook.com>
2025-06-03 01:30:09 -0300

    feat: added conform for code formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+	'stevearc/conform.nvim',
+
+	config = function()
+		require 'conform'.setup {
+			formatters_by_ft = {
+				json = { 'prettier' },
+				scss = { 'prettier' },
+				html = { 'prettier' },
+				markdown = { 'prettier' },
+				htmlangular = { 'prettier' },
+				javascript = { 'prettier' },
+				typescript = { 'prettier' },
+			},
+			format_on_save = {
+				lsp_format = "never",
+			}
+		}
+	end,
+}
