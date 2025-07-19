fe4b0ad5a1dd2d0cb28cad72d052a48cd137d81f
breno-ca <breno_de_avila@outlook.com>
2025-07-19 14:52:36 -0300

    change: keep settings for copilot and local llm returning copilot as default

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,10 +1,37 @@
-return {
+local github_copilot = {
 	'CopilotC-Nvim/CopilotChat.nvim',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	build = 'make tiktoken',
 
-	dependencies = {
-		{ 'nvim-lua/plenary.nvim' }
-	},
+	opts = {
+		question_header = '## Breno ',
+		window = {
+			layout = 'float',
+			width = 0.8,
+			height = 0.8,
+			relative = 'editor',
+			border = 'rounded',
+			row = 3,
+		},
+		selection = function(source)
+			local select = require 'CopilotChat.select'
+			local empty_out = {
+				bufnr = 0,
+				content = '',
+				diagnostics = nil,
+				end_line = 0,
+				filename = '',
+				filetype = '',
+				start_line = 1,
+			}
+			return select.visual(source) or empty_out
+		end,
+	}
+}
 
+local local_llm = {
+	'CopilotC-Nvim/CopilotChat.nvim',
+	dependencies = { 'nvim-lua/plenary.nvim' },
 	build = 'make tiktoken',
 
 	opts = function()
@@ -38,34 +65,31 @@ return {
 				}
 			)
 			if err then error(err) end
-
 			return response.body.data
 		end
-
-		local get_url = function()
-			return 'http://localhost:11434/chat/completions'
-		end
-
+		local get_url = function() return 'http://localhost:11434/chat/completions' end
 		local get_models = function(headers)
 			local response, err = utils.curl_get(
 				'http://localhost:11434/models',
 				{ headers = headers, json_response = true }
 			)
 			if err then error(err) end
-
-			return vim.tbl_map(function(model)
-				return { id = model.id, name = model.id }
-			end, response.body.data)
+			return vim.tbl_map(function(model) return { id = model.id, name = model.id } end, response.body.data)
 		end
-
 		return {
-
 			model = ggufs .. model,
-
 			system_prompt = system_prompt,
 
-			-- returning a empty out fix the
-			-- highlight in chat buffer
+			question_header = '## Breno ',
+			window = {
+				title = '| llama.cpp Chat | ' .. model .. ' |',
+				layout = 'float',
+				relative = 'editor',
+				width = 0.8,
+				height = 0.8,
+				border = 'rounded',
+				row = 3,
+			},
 			selection = function(source)
 				local empty_out = {
 					bufnr = 0,
@@ -78,27 +102,11 @@ return {
 				}
 				return select.visual(source) or empty_out
 			end,
-
-			question_header = '## Breno ',
-
-			window = {
-				layout = 'float',
-				width = 0.8,
-				height = 0.8,
-
-				relative = 'editor',
-				border = 'rounded',
-				row = 3,
-				title = '| llama.cpp Chat | ' .. model .. ' |',
-			},
-
 			providers = {
 				github_models = { disabled = true },
-
 				ollama = {
 					prepare_input = providers.copilot.prepare_input,
 					prepare_output = providers.copilot.prepare_output,
-
 					embed = embed,
 					get_url = get_url,
 					get_models = get_models,
@@ -107,3 +115,5 @@ return {
 		}
 	end
 }
+
+return github_copilot
