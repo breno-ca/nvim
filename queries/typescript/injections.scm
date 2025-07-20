cefaae6c011e317d9c09285a140b071f4354d0b3
breno-ca <breno_de_avila@outlook.com>
2025-07-20 01:26:26 -0300

    feat: added embedded sql syntax highlight in typescript files

--- queries/typescript/injections.scm
+++ queries/typescript/injections.scm
@@ -1,11 +1,10 @@
-;HTML highlight injection for string literals
-;(
-;  (pair
-;    key: (property_identifier) @template
-;    value: (template_string
-;      (string_fragment) @injection.content
-;    )
-;  )
-;  (#eq? @template "template")
-;  (#set! injection.language "html")
-;)
+;SQL highlight injection for string literals
+(
+  (template_string
+    (string_fragment) @injection.content
+    (#match? @injection.content
+      "(CREATE|ALTER|DROP|SELECT|INSERT|REPLACE|UPDATE|DELETE|WHERE|JOIN|ORDER).+(TABLE|FROM|VALUES|SET|ON|BY)?"
+    )
+    (#set! injection.language "sql")
+  )
+)
