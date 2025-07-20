4154dc225592515cd1c28c32c2ba23cb18ffce90
breno-ca <breno_de_avila@outlook.com>
2025-07-20 01:09:13 -0300

    feat: added dbee as the default database client

---
+++ lua/plugins/nvim-dbee.lua
@@ -0,0 +1,9 @@
+return {
+	'kndndrj/nvim-dbee',
+	dependencies = {
+		'MunifTanjim/nui.nvim',
+	},
+
+	build = function() require 'dbee'.install('curl') end,
+	config = function() require 'dbee'.setup() end,
+}
