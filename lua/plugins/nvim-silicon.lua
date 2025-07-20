3063ea28f5a5a0799cd146ba4f9e99a52c068234
breno-ca <breno_de_avila@outlook.com>
2025-07-20 01:15:30 -0300

    feat: added nvim-silicon to create beautiful code images from visual selections

---
+++ lua/plugins/nvim-silicon.lua
@@ -0,0 +1,13 @@
+return {
+	'michaelrommel/nvim-silicon',
+	lazy = true,
+	cmd = 'Silicon',
+	opts = {
+		disable_defaults = false,
+		shadow_color = nil,
+		to_clipboard = true,
+		wslclipboard = 'always',
+		wslclipboardcopy = 'keep',
+		command = 'silicon',
+	}
+}
