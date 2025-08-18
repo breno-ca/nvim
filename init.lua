b243c1e5ab3b34a020ac70ec216e30ec9477eeb6
breno-ca <breno_de_avila@outlook.com>
2025-08-18 18:13:27 -0300

    chore: updated init.lua setup files

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
--- configurações do nvim
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.laststatus = 3
 vim.opt.termguicolors = true
 vim.opt.fillchars = { eob = " " }
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.g.floaterm_height = 0.9
 vim.g.floaterm_width = 0.9
 vim.g.python3_host_prog = os.getenv('HOME') .. '/python3/bin/python3.13'
@@ -14,50 +15,31 @@ vim.cmd('set linebreak')
 vim.cmd('set wrap!')
 vim.cmd('set number')
 vim.cmd("let mapleader = ' '")
-
--- opções de inicialização
 vim.cmd('autocmd VimEnter * NvimTreeOpen')
 vim.cmd('autocmd VimEnter * wincmd w')
 
--- configurações recomendadas para o nvim-tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- adição de filetypes
+-- filetype extras
 vim.filetype.add({
-	filename = {
-		['docker-compose.yml'] = 'yaml.docker-compose',
-	}
+  filename = {
+    ['docker-compose.yml'] = 'yaml.docker-compose',
+  }
 })
 
--- lazy.nvim para gerenciamento de plugins
+-- lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- carrega os plugins da pasta
+-- init
 require 'lazy'.setup 'plugins'
-
--- carrega os atalhos globais personalizados
 require 'shortcuts'
-
--- carrega as funções para alterar a fonte
-require 'fonts'
-
--- carrega os setups necessários
-require 'setup'
-
--- carrega o suporte a linguagens
-require 'setup-lsp'
-
--- carrega as configurações de debug
--- require 'setup-dap'
+require 'lsp'
