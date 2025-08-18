b867596af6c492f2811d590a1e7cb47bacaa10c9
breno-ca <breno_de_avila@outlook.com>
2025-08-18 18:05:22 -0300

    chore: added react filetypes to work with prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,21 +1,23 @@
 return {
-	'stevearc/conform.nvim',
+  'stevearc/conform.nvim',
 
-	config = function()
-		require 'conform'.setup {
-			formatters_by_ft = {
-				css = { 'prettier' },
-				json = { 'prettier' },
-				scss = { 'prettier' },
-				html = { 'prettier' },
-				markdown = { 'prettier' },
-				htmlangular = { 'prettier' },
-				javascript = { 'prettier' },
-				typescript = { 'prettier' },
-			},
-			format_on_save = {
-				lsp_format = "never",
-			}
-		}
-	end,
+  config = function()
+    require 'conform'.setup {
+      formatters_by_ft = {
+        css = { 'prettier' },
+        json = { 'prettier' },
+        scss = { 'prettier' },
+        html = { 'prettier' },
+        markdown = { 'prettier' },
+        htmlangular = { 'prettier' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+      },
+      format_on_save = {
+        lsp_format = 'never',
+      }
+    }
+  end,
 }
