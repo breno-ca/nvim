2f25cf6a0d517f86741bad483934b14ded0302d6
breno-ca <breno_de_avila@outlook.com>
2025-09-16 20:34:00 -0300

    refactor: improved built-in lsp settings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,236 +5,182 @@ if ok_cmp then
   capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 end
 
--- root_pattern helper
-local function root_pattern(...)
-  local patterns = { ... }
-  return function(fname)
-    return vim.fs.dirname(vim.fs.find(patterns, { upward = true, path = fname })[1])
-        or vim.fn.getcwd()
-  end
-end
-
 -- gopls
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'go', 'gomod', 'gowork', 'gotmpl' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'gopls',
-      cmd = { 'gopls' },
-      root_dir = root_pattern('go.work', 'go.mod', '.git')(args.file),
-      capabilities = capabilities,
-      settings = {
-        gopls = {
-          analyses = { nilness = true, unusedparams = true },
-          completeUnimported = true,
-          gofumpt = true,
-          staticcheck = true,
-          hints = {
-            assignVariableTypes = true,
-            parameterNames = true,
-            compositeLiteralFields = true,
-            compositeLiteralTypes = true,
-          },
-        },
+vim.lsp.config.gopls = {
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  cmd = { 'gopls' },
+  root_markers = { 'go.work', 'go.mod' },
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      analyses = { nilness = true, unusedparams = true },
+      completeUnimported = true,
+      gofumpt = true,
+      staticcheck = true,
+      hints = {
+        assignVariableTypes = true,
+        parameterNames = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
       },
-    })
-  end,
-})
+    },
+  },
+}
+vim.lsp.enable('gopls')
 
 -- phpactor
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'php' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'phpactor',
-      cmd = { 'phpactor', 'language-server' },
-      root_dir = vim.fn.getcwd(),
-      capabilities = capabilities,
-      settings = {
-        phpactor = {
-          completion = { enabled = true },
-          monorepo = { enabled = true },
-          phpstan = { enabled = true },
-          psalm = { enabled = true },
-          xdebug = { enabled = true },
+vim.lsp.config.phpactor = {
+  filetypes = { 'php' },
+  cmd = { 'phpactor', 'language-server' },
+  root_markers = { '.phpactor.json', 'composer.json' },
+  capabilities = capabilities,
+  settings = {
+    phpactor = {
+      completion = { enabled = true },
+      monorepo = { enabled = true },
+      phpstan = { enabled = true },
+      psalm = { enabled = true },
+      xdebug = { enabled = true },
+    },
+  },
+}
+vim.lsp.enable('phpactor')
+
+-- luals
+vim.lsp.config.luals = {
+  filetypes = { 'lua' },
+  cmd = { 'lua-language-server' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', 'init.lua' },
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        enable = true,
+        globals = { 'vim', 'nvim' },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
         },
       },
-    })
-  end,
-})
-
--- lua_ls
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'lua' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'lua_ls',
-      cmd = { 'lua-language-server' },
-      root_dir = root_pattern('.git')(args.file),
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            enable = true,
-            globals = { 'vim', 'nvim' },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-              [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-            },
-          },
-          format = {
-            enable = true,
-            defaultConfig = {
-              indent_style = "space",
-              indent_size = "2",
-            },
-          },
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = "space",
+          indent_size = "2",
         },
       },
-    })
-  end,
-})
-
--- cssls
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'css', 'scss' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'cssls',
-      cmd = { 'vscode-css-language-server', '--stdio' },
-      root_dir = root_pattern('.git')(args.file),
-      capabilities = capabilities,
-    })
-  end,
-})
-
--- tailwindcss
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'css', 'scss', 'html', 'javascript', 'typescript', 'typescriptreact' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'tailwindcss',
-      cmd = { 'tailwindcss-language-server', '--stdio' },
-      root_dir = root_pattern('tailwind.config.js', 'tailwind.config.cjs', 'tailwind.config.ts', '.git')(args.file),
-      capabilities = capabilities,
-    })
-  end,
-})
+    },
+  },
+}
+vim.lsp.enable('luals')
 
 -- pyright
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'python' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'pyright',
-      cmd = { 'pyright-langserver', '--stdio' },
-      root_dir = root_pattern('requirements.txt', '.git')(args.file),
-      capabilities = capabilities,
-      settings = {
-        python = {
-          analysis = {
-            autoSearchPaths = true,
-            useLibraryCodeForTypes = true,
-          },
-          workspaceSymbols = {
-            detectRelativeImports = true,
-          },
-        },
+vim.lsp.config.pyright = {
+  filetypes = { 'python' },
+  cmd = { 'pyright-langserver', '--stdio' },
+  root_markers = { 'requirements.txt' },
+  capabilities = capabilities,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
       },
-    })
-  end,
-})
+      workspaceSymbols = {
+        detectRelativeImports = true,
+      },
+    },
+  },
+}
+vim.lsp.enable('pyright')
 
 -- ts_ls (typescript/javascript)
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'javascript', 'typescript', 'typescriptreact' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'ts_ls',
-      cmd = { 'typescript-language-server', '--stdio' },
-      root_dir = root_pattern('package.json', 'tsconfig.json', '.git')(args.file),
-      capabilities = capabilities,
-      on_attach = function(client)
-        client.server_capabilities.documentFormattingProvider = false
-      end,
-    })
+vim.lsp.config.ts_ls = {
+  filetypes = { 'javascript', 'typescript', 'typescriptreact' },
+  cmd = { 'typescript-language-server', '--stdio' },
+  root_markers = { 'package.json', 'tsconfig.json' },
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false
   end,
-})
+}
+vim.lsp.enable('ts_ls')
 
--- bashls
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'sh', 'bash' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'bashls',
-      cmd = { 'bash-language-server', 'start' },
-      root_dir = root_pattern('.git')(args.file),
-      capabilities = capabilities,
-      init_options = {
-        format = {
-          enable = true,
-        },
-      },
-    })
-  end,
-})
+-- tailwindcss
+vim.lsp.config.tailwindcss = {
+  filetypes = { 'typescriptreact', 'javascriptreact' },
+  cmd = { 'tailwindcss-language-server', '--stdio' },
+  root_markers = { { 'tailwind.config.js', 'tailwind.config.cjs', 'tailwind.config.ts' }, '.git' },
+  capabilities = capabilities,
+}
+vim.lsp.enable('tailwindcss')
+
+-- cssls
+vim.lsp.config.cssls = {
+  filetypes = { 'css', 'scss' },
+  name = 'cssls',
+  cmd = { 'vscode-css-language-server', '--stdio' },
+  root_markers = { 'package.json', 'css.config.js', 'css.config.cjs', 'css.config.ts', '.git' },
+  capabilities = capabilities,
+}
 
 -- jsonls
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'json' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'jsonls',
-      cmd = { 'vscode-json-language-server', '--stdio' },
-      root_dir = root_pattern('.git')(args.file),
-      capabilities = capabilities,
-    })
-  end,
-})
+vim.lsp.config.jsonls = {
+  filetypes = { 'json' },
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  capabilities = capabilities,
+}
+vim.lsp.enable('jsonls')
+
+-- bashls
+vim.lsp.config.bashls = {
+  filetypes = { 'sh', 'bash' },
+  name = 'bashls',
+  cmd = { 'bash-language-server', 'start' },
+  root_markers = { '.git' },
+  capabilities = capabilities,
+  init_options = {
+    format = {
+      enable = true,
+    },
+  },
+}
+vim.lsp.enable('bashls')
 
 -- sqls
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'sql' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'sqls',
-      cmd = { 'sqls' },
-      root_dir = root_pattern('.git')(args.file),
-      capabilities = capabilities,
-      on_attach = function(client)
-        client.server_capabilities.documentFormattingProvider = false
-      end,
-    })
+vim.lsp.config.sqls = {
+  filetypes = { 'sql' },
+  name = 'sqls',
+  cmd = { 'sqls' },
+  root_markers = { '.git' },
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false
   end,
-})
+}
+vim.lsp.enable('sqls')
 
 -- dockerls
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'dockerfile' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'dockerls',
-      cmd = { 'docker-langserver', '--stdio' },
-      root_dir = root_pattern('.git')(args.file),
-      capabilities = capabilities,
-    })
-  end,
-})
+vim.lsp.config.dockerls = {
+  filetypes = { 'dockerfile' },
+  name = 'dockerls',
+  cmd = { 'docker-langserver', '--stdio' },
+  root_markers = { 'Dockerfile', '.git' },
+  capabilities = capabilities,
+}
+vim.lsp.enable('dockerls')
 
 -- docker compose language service
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'yaml.docker-compose' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'docker_compose_language_service',
-      cmd = { 'docker-compose-langserver', '--stdio' },
-      root_dir = root_pattern('.git')(args.file),
-      capabilities = capabilities,
-    })
-  end,
-})
+vim.lsp.config.docker_compose_language_service = {
+  filetypes = { 'yaml.docker-compose' },
+  name = 'docker_compose_language_service',
+  cmd = { 'docker-compose-langserver', '--stdio' },
+  root_markers = { 'docker-compose.yml', 'docker-compose.yaml', '.git' },
+  capabilities = capabilities,
+}
+vim.lsp.enable('docker_compose_language_service')
 
 -- global format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -251,3 +197,14 @@ end, { desc = 'Restart all LSP clients' })
 vim.api.nvim_create_user_command('LspInfo', function()
   vim.cmd('checkhealth vim.lsp')
 end, { desc = 'Show info about running LSPs' })
+
+-- diagnostics
+local config = vim.diagnostic.config
+config({ virtual_text = { current_line = true } })
+
+keymap(n, '<Leader>ve', call(function()
+  if (not config().virtual_lines) then
+    config({ virtual_lines = true, virtual_text = false }); return
+  end
+  config({ virtual_lines = false, virtual_text = { current_line = true } })
+end), { desc = 'Toggle virtual lines/text diagnostics' })
