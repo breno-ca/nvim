b5ea1d2ba74e20839d36decc38933a7d19713894
breno-ca <breno_de_avila@outlook.com>
2025-11-28 10:11:32 -0300

    chore: updated leap config to new standards

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,6 +2,6 @@ return {
   'ggandor/leap.nvim',
   lazy = false,
   config = function()
-    require 'leap'.create_default_mappings()
+    require 'leap'.opts.vim_opts['go.ignorecase'] = true
   end
 }
